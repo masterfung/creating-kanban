b21f94956a96ec59385807f525e8db5da0cfda54
Tsung Hung <hungmasterj@gmail.com>
2016-02-22 18:53:46 -0800

    progress learning webpack

---
+++ app/component.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+  var element = document.createElement('h1');
+
+  element.innerHTML = 'Howdy World to Me!';
+  return element;
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    build: PATHS.build,
+    filename: 'bundle.js'
+  }
+}
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
