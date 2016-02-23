f8d97df9fc4d6c4133d9c90849357f3c21b91871
Tsung Hung <hungmasterj@gmail.com>
2016-02-23 08:09:21 -0800

    added env start and also updated env usage for HMR

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
 
+process.env.BABEL_ENV = TARGET;
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
