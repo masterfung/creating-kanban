4894d4a5a724242480916237d12a72066baac930
Tsung Hung <hungmasterj@gmail.com>
2016-02-26 20:52:49 -0800

    added new drag-n-drop and also began implementation

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,5 +1,7 @@
 import AltContainer from 'alt-container';
 import React from 'react';
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 import Header from './Header';
 
@@ -7,6 +9,7 @@ import Lanes from './Lanes';
 import LaneStore from '../stores/LaneStore';
 import LaneActions from '../actions/LaneActions';
 
+@DragDropContext(HTML5Backend)
 export default class App extends React.Component {
   render() {
     return (
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,72 +1,7 @@
 import React from 'react';
 
 export default class Note extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      editing: false
-    };
-  }
-
   render() {
-    if(this.state.editing) {
-      return this.renderEdit();
-    }
-
-    return this.renderNote();
+    return <li {...this.props}>{this.props.children}</li>
   }
-
-  renderEdit = () => {
-    return <input type="text"
-      ref={
-        (e) => e ? e.selectionStart = this.props.task.length : null
-      }
-      autoFocus={true}
-      defaultValue={this.props.task}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter} />;
-  }
-
-  renderDelete = () => {
-    return <button
-      className="delete-note"
-      onClick={this.props.onDelete}>x</button>;
-  }
-
-  renderNote = () => {
-    const onDelete = this.props.onDelete;
-
-    return (
-      <div onClick={this.edit}>
-        <span className="task">{this.props.task}</span>
-        {onDelete ? this.renderDelete() : null }
-      </div>
-    );
-  }
-
-  edit = () => {
-    this.setState({
-      editing: true
-    });
-  }
-
-  checkEnter = (e) => {
-    if(e.key === 'Enter') {
-      this.finishEdit(e);
-    }
-  }
-  
-  finishEdit = (e) => {
-    const value = e.target.value;
-
-    if(this.props.onEdit) {
-      this.props.onEdit(value);
-
-      // Exit edit mode.
-      this.setState({
-        editing: false
-      });
-    }
-  };
 }
