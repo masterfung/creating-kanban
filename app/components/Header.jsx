01eb5701106cf92968439b78853ea581e764078a
Tsung Hung <hungmasterj@gmail.com>
2016-04-28 23:00:28 -0700

    updated lane information

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,18 +3,20 @@ import LaneActions from '../actions/LaneActions';
 
 export default class Header extends React.Component {
   render() {
-    return   <nav>
-    <div className="nav-wrapper">
-      <a href="#" className="brand-logo">I Kan Band</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href='#' onClick={this.addLane}
-        className='btn red darken-2'>Add Lane</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">I Kan Band</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href='#' onClick={this.addLane}
+            className='btn red darken-2'>Add Lane</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
   }
   addLane() {
-    LaneActions.create({name: 'New Lane Topic Name'});
+    LaneActions.create({name: 'Topic Name'});
   }
 }
