e9e2560f53b2e326a9e65c0fbb11a315e8423027
Tsung Hung <hungmasterj@gmail.com>
2016-05-01 01:50:41 -0700

    added footer component and also updated index

---
+++ app/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="page-footer">
+          <div className="footer-copyright">
+            <div className="container">
+            © 2016 I Kan Band | Tsung Hung
+            
+            <a className="grey-text text-lighten-4 right"
+            href="https://materializecss.com">Powered by Materialize</a>
+            </div>
+          </div>
+        </footer>
+    )
+  }
+}
