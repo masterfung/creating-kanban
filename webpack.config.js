89ec903f9de70b60ce721290665c37396d3adf2f
Tsung Hung <hungmasterj@gmail.com>
2016-05-03 01:02:54 -0700

    changed the overall webpack to use a different setup-WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,47 @@
-const path = require('path');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
+'use strict';
 
-const NpmInstallPlugin = require('npm-install-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const TARGET = process.env.npm_lifecycle_event;
-
-process.env.BABEL_ENV = TARGET;
-
-const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
-};
-
-const common = {
-  entry: {
-    app: PATHS.app
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'app/main.js')
+  ],
   output: {
-    build: PATHS.build,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: PATHS.app
-      },
-      {
-        test: /\.jsx?$/,
-        loaders: [
-          'babel?cacheDirectory,presets[]=react,presets[]=es2015,presets[]=survivejs-kanban'
-        ],
-        include: PATHS.app
-      },
-      // SASS
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass'
-      }
-    ]
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
   },
   plugins: [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress:{
-      warnings: true
-    }
-  })
-  ]
-}
-
-if (TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {
-    devtool: 'eval-source-map',
-    devServer: {
-      contentBase: PATHS.build,
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      progress: true,
-      stats: 'errors-only',
-      host: process.env.HOST,
-      port: process.env.PORT
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new NpmInstallPlugin({
-        save: true
-      })
-    ]
-  });
-}
-
-if (TARGET === 'build') {
-  module.exports = merge(common, {});
-}
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        "presets": ["react", "es2015", "stage-0", "react-hmre", "survivejs-kanban"]
+      }
+    }, {
+      test: /\.json?$/,
+      loader: 'json'
+    }, {
+      test: /\.scss$/,
+      loader: 'style!css!sass'
+    }]
+  }
+};
