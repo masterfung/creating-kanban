0235f6cbfde0713dfce3045aa2b6555be7dcccab
Tsung Hung <hungmasterj@gmail.com>
2016-05-05 16:16:54 -0400

    moved the placement around to insure no css conflicts

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,7 @@ import AltContainer from 'alt-container';
 import React from 'react';
 import {DragDropContext} from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+// import Firebase from 'firebase';
 
 import Header from './Header';
 import Footer from './Footer';
@@ -10,16 +11,17 @@ import Lanes from './Lanes';
 import LaneStore from '../stores/LaneStore';
 import LaneActions from '../actions/LaneActions';
 
+// let firebaseRef = new Firebase('https://ikanband.firebaseio.com/');
+
 @DragDropContext(HTML5Backend)
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
         <div className="Site">
           <main className="Site-content">
-            <div className="container kanban-app">
-
+            <Header />
+            <div className="container">
               <AltContainer
                 stores={[LaneStore]}
                 inject={{
